686da774cfb461c2df715c9cd4af44f28f33f528
Chie Shikanai Eklund <shikachie@ChienoMacBook-Air.local>
2025-10-30 14:23:23 +0100

    Fix: Remove unused variable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const unusedVariable = "hello"; 
   return (
     <div className="App">
       <header className="App-header">
